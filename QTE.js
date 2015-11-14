086eccf532dd5ae242e27063e3b9c8ad5f51ef6a
AndyLonly <liujiaqisz@me.com>
2015-11-14 23:23:36 +0800

    Update QTE.js

--- QTE.js
+++ QTE.js
@@ -1,10 +1,11 @@
 QTE=new Object();
 QTE.Time() = function(time) {
- var t=setTimeout(QTE. couse,This.time)
+ var t=setTimeout(QTE.couse,This.time)
 }
-QTE.key() = function(first,second,,) {
+QTE.key() = function(first,second,T,N) {
  Var F = This.first
  Var S = This.second
- Var T = This.
- Var
+ Var T = This.T
+ Var N = This.N
 }
+QTE.couse()=
