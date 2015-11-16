8d5f366d8abc3934ad833c4ba601404a7a903520
AndyLonly <liujiaqisz@me.com>
2015-11-16 22:04:26 +0800

    Update QTE.js

--- QTE.js
+++ QTE.js
@@ -14,11 +14,21 @@ if (!this.keyd){
     }else{run = 2}
   }else{ run = 3 }
 }else{ run = 4 }
+/*
+下面这一坨用来随机取值来得到一个随机的KEY
+*/
 keyn = Math.ceil(Math.random()* run)
-
+/*
+下面这一坨把准备好按键的进行对照以后从数字到按键，然而..........神他妈按键怎么弄卧槽
+*/
 if (keyn =1)
-function even
+/*
+下面这一坨用来写RPGMAKER的API or 事件吧，等我看完RPGMAKER的说明书再说ouo
+*/
 
 
 
+/*
+结尾撒花~
+*/
 }
