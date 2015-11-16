cbe784764636c5a03f52386e04dd82e28eee4335
AndyLonly <liujiaqisz@me.com>
2015-11-16 16:52:08 +0800

    Create QTE.js

---
+++ QTE.js
@@ -0,0 +1,24 @@
+/*
+
+
+*/
+function QTE(keya,keyb,keyc,keyd,time)
+{
+/*
+下面这一坨用来准备好按键的随机运算
+*/
+if (!this.keyd){
+  if (!this.keyc){
+    if (!this.keyb){
+        run = 1
+    }else{run = 2}
+  }else{ run = 3 }
+}else{ run = 4 }
+keyn = Math.ceil(Math.random()* run)
+
+if (keyn =1)
+function even
+
+
+
+}
