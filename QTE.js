cbdfc06bc8066684b41ec18db48c61d2f11c9ee3
AndyLonly <liujiaqisz@me.com>
2015-11-23 16:04:20 +0800

    Update 0.01.2
    框架完成

--- QTE.js
+++ QTE.js
@@ -23,9 +23,25 @@ if (!this.keyd){
 */
 keyn = Math.ceil(Math.random()* run)
 /*
-下面这一坨把准备好按键的进行对照以后从数字到按键，然而..........神他妈按键怎么弄卧槽
+下面这一坨把准备好按键的进行对照以后从数字到按键，然而..........神他妈按键怎么弄卧槽,保留按键。
 */
-if (keyn =1)
+switch(n)
+{
+case 1:
+  input = 
+  break;
+case 2:
+  input =
+  break;
+case 3:
+  input =
+  break;
+case 4:
+  input =
+  break;
+default:
+  pass = 1;
+}
 /*
 下面这一坨用来写RPGMAKER的API or 事件吧，等我看完RPGMAKER的说明书再说ouo
 */
